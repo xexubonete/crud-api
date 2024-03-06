a1bbebba7067990909dcf1b4cbea222fad248d6b
xexubonete <xexubonete@gmail.com>
2024-03-06 18:08:29 +0100

    Create ClientService

---
+++ ASP.NET WebApi/Services/ClientService.cs
@@ -0,0 +1,40 @@
+﻿using ASP.NET_WebApi.Entities;
+using ASP.NET_WebApi.Interfaces;
+using ASP.NET_WebApi.Repositories;
+
+namespace ASP.NET_WebApi.Services
+{
+    public class ClientService : IClientService
+    {
+        private readonly ClientRepository _repository;
+        public Task<Client> CreateClient(Client newClient)
+        {
+            return _repository.CreateClient(newClient);
+        }
+
+        public Task<Client> DeleteClientById(Guid id)
+        {
+            return _repository.DeleteClientById(id);
+        }
+
+        public Task<IList<Client>> GetAllClients()
+        {
+            return _repository.GetAllClients();
+        }
+
+        public Task<Client> GetClientById(Guid id)
+        {
+            return _repository.GetClientById(id);
+        }
+
+        public Task<IList<Client>> GetClientByName(string name)
+        {
+            return _repository.GetClientByName(name);
+        }
+
+        public Task<Client> UpdateClientById(Client updateClient)
+        {
+            return _repository.UpdateClientById(updateClient);
+        }
+    }
+}
