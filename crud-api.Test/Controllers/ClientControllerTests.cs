de0846b10283af38a1b75418c2aa2cb4cfbc533e
xexubonete <xexubonete@gmail.com>
2024-06-17 16:38:06 +0200

    [TEST] Created new ClientControllerTests

---
+++ crud-api.Test/Controllers/ClientControllerTests.cs
@@ -0,0 +1,118 @@
+namespace ClientControllerTests
+{
+    public class ClientControllerTests
+    {
+        public ClientControllerTests()
+        {
+        }
+
+        [Fact]
+        public void ClientController_CreateClient_Should_Return_Ok()
+        {
+            // Given
+        
+            // When
+        
+            // Then
+        }
+        [Fact]
+        public void ClientController_CreateClient_Should_Return_BadRequest()
+        {
+            // Given
+        
+            // When
+        
+            // Then
+        }
+        [Fact]
+        public void ClientController_GetAllClients_Should_Return_All_Clients()
+        {
+            // Given
+        
+            // When
+        
+            // Then
+        }
+        [Fact]
+        public void ClientController_GetAllClients_Should_Return_NotFound()
+        {
+            // Given
+        
+            // When
+        
+            // Then
+        }
+        [Fact]
+        public void ClientController_GetClientById_Should_Return_Client_By_Id()
+        {
+            // Given
+        
+            // When
+        
+            // Then
+        }
+        [Fact]
+        public void ClientController_GetClientById_Should_Return_NotFound()
+        {
+            // Given
+        
+            // When
+        
+            // Then
+        }
+        [Fact]
+        public void ClientController_GetClientByName_Should_Return_Client_By_Name()
+        {
+            // Given
+        
+            // When
+        
+            // Then
+        }
+        [Fact]
+        public void ClientController_GetClientByName_Should_Return_NotFound()
+        {
+            // Given
+        
+            // When
+        
+            // Then
+        }
+        [Fact]
+        public void ClientController_UpdateClient_Should_Return_Updated_Client()
+        {
+            // Given
+        
+            // When
+        
+            // Then
+        }
+        [Fact]
+        public void ClientController_UpdateClient_Should_Return_NotFound()
+        {
+            // Given
+        
+            // When
+        
+            // Then
+        }
+        [Fact]
+        public void ClientController_DeleteClient_Should_Return_Deleted_Client()
+        {
+            // Given
+        
+            // When
+        
+            // Then
+        }
+        [Fact]
+        public void ClientController_DeleteClient_Should_Return_NotFound()
+        {
+            // Given
+        
+            // When
+        
+            // Then
+        }
+    }
+}
